ec9e81a39fda059a29e2fa28a25795c09c0d7e77
Nivyox <niek@keersop.nl>
2017-01-27 10:19:07 +0100

    Push it like uhmmm... idk

--- src/me/nivyox/duels/listeners/PlayerListener.java
+++ src/me/nivyox/duels/listeners/PlayerListener.java
@@ -5,9 +5,12 @@ import me.nivyox.duels.game.Game;
 import me.nivyox.duels.game.GameManager;
 import me.nivyox.duels.game.GameType;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -26,7 +29,7 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        if(GameManager.isIngame(player)) {
+        if (GameManager.isIngame(player)) {
             Game game = GameManager.getGame(player);
             game.endGame(EndReason.OPPONENT_LEFT);
             game.removePlayer(player);
@@ -35,15 +38,38 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onHealthRegen(EntityRegainHealthEvent event) {
-        if(event.getEntity() instanceof Player) {
+        if (event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
-            if(GameManager.isIngame(player)) {
+            if (GameManager.isIngame(player)) {
                 Game game = GameManager.getGame(player);
-                if(game.getType() == GameType.UHC) {
+                if (game.getType() == GameType.UHC) {
                     event.setCancelled(true);
                 }
             }
         }
     }
 
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void entityDamagebyEntity(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player) {
+            if (event.getDamager() instanceof Player) {
+                Player player = (Player) event.getEntity();
+                Player damager = (Player) event.getDamager();
+                if (GameManager.getGame(player).getPlayers().contains(damager) || damager.getGameMode() == GameMode.CREATIVE) {
+                } else {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
 }
